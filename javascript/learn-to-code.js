8d0cf325af4c83380461d6441faa010d613681d8
Chris Jao <jao.chris@gmail.com>
2017-12-20 07:10:20 -0700

    javascript basic numbers

---
+++ javascript/learn-to-code.js
@@ -0,0 +1,38 @@
+//NOTES:
+//variable(var) is a like a bucket
+//and in that bucket has something in it 
+//and that something is a "strings"
+
+
+//Sample
+//var name = "Chris";
+//var age = 29;
+//
+//var message = "Hi, My Name is " + name + " and I am " + age + " Years old";
+
+//var firstName = "John";
+//var lastName = "something";
+//var dateOfBirth = "10-09-82";
+//var age = 23;
+//var profileImgUrl = "https://media0.giphy.com/media/S2S0ZDytY6yDm/giphy.gif"
+//
+//var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday!"; 
+//
+//console.log(loginWelcomeMessage);
+
+var sum = 10 + 15;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var div = 10 / 3;
+var mod = 10 % 3;
+var msg = "10 / 3 = 3 with a remainder of " + mod;
+var result = 10 * ((5 + 3) - 4);
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(mod);
+console.log(msg);
+console.log(result);
+
